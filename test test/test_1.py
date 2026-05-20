95eb3cbe514ecb5204273a82375e304300143608
User <jamees941022@gmail.com>
2026-05-20 17:25:38 +0800

    test

---
+++ test test/test_1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.DataFrame({
+
+    "name": ["Amy", "Bob", "Cathy", "David", "Evan", "Fiona"],
+
+    "age": [20, 21, 19, 22, 20, 23],
+
+    "score": [88, 92, 79, 85, 90, 95]
+
+})
+
+print(df.describe().round(2))
+#查看後5筆
+
